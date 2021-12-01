dd3b01874e0a05c400e7ffed54ac778748f69ddf
Townes <59765081+GeneralTownes@users.noreply.github.com>
2021-12-01 01:13:32 +0000

    Class Update
    Used classes for the first time

--- MareNostrum.py
+++ MareNostrum.py
@@ -2,6 +2,7 @@
 
 import pygame as pg
 import numpy as np
+import os
 
 #initialisation
 pg.init()
@@ -13,27 +14,30 @@ pg.display.set_caption('Mare Nostrum')
 clock = pg.time.Clock()
 
 
-islandSummer = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\town\background\rank_5\Capital_R5.jpg')
+islandSummer = pg.image.load(r'C:\\Program Files (x86)\\MareNostrum\\gfx\\town\\background\\rank_5\\Capital_R5.jpg')
 
+homeDirectory = os.getcwd()
 
 
 def Background(x,y):
     gameDisplay.blit(islandSummer, (x,y))
 
 
+class Building:
+    def __init__(self, buildingType, name, location, level):
+        self.buildingType = buildingType
+        self.name = name
+        self.location = location
+        self.level = level
+        
+    def buildingGFX (self):
+        buildingImage = pg.image.load( os.path.join(homeDirectory, 'gfx\\town\\buildings\\', self.name + '.png') ).convert_alpha()
+        return buildingImage
 
-
-Background(0,0)
-
-
-
-townHall = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\town\buildings\TownHall.png').convert_alpha()
-
-gameDisplay.blit(townHall, (840,400)) # paint to screen
-pg.display.flip() # paint screen one time
+townHall = Building('standard','townHall',(838,398), 1)
 
 #clickable area
-click_rect  = townHall.get_rect()
+click_rect  = townHall.buildingGFX().get_rect()
 
 crashed = False
 
@@ -47,30 +51,26 @@ while not crashed:
         elif ( event.type == pg.MOUSEBUTTONUP ):
            # mouse_position = pg.mouse.get_pos()             # Location of the mouse-click
             if ( click_rect.collidepoint( np.subtract(mouse_position, (840,400) ) ) ):   # Was that click inside our rectangle 
-                #print( "hit" )
                 display_width = 1386
                 display_height = 924
                 gameDisplay = pg.display.set_mode((display_width,display_height))
                 islandSummer = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\island\background\Island1_Summer(1386x924).jpg')
                 
             else:
-                print( "click-outside!" )
+                pass
         
         elif ( click_rect.collidepoint( np.subtract(mouse_position, (840,400) ) ) ):
-            print( "hover" )
-            gameDisplay.blit(townHall, (838,398)) # paint to screen
+            gameDisplay.blit(townHall.buildingGFX(), (838,398)) # paint to screen
             townHall_glow = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\town\buildings\Glow\townHallGlow.png').convert_alpha()
             gameDisplay.blit(townHall_glow, (838,398)) # paint to screen
         
         else:
-            print( "not hover" )
-            townHall = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\town\buildings\TownHall.png').convert_alpha()
-            gameDisplay.blit(townHall, (838,398)) # paint to screen
+            gameDisplay.blit(townHall.buildingGFX(), (838,398)) # paint to screen
             
-        
-       # print (event)
         
     pg.display.update()
     clock.tick(60)
+
+
 
 pg.quit()
---
+++ test.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 23:39:24 2021
+
+@author: James
+"""
+name = 'townHall'
+
+homeDirectory = "C:\\Program Files (x86)\\MareNostrum\gfx\\town\\buildings\\"
+imageLocation = "r'" + homeDirectory + name + ".png'"
+print(homeDirectory)
+print(imageLocation)
