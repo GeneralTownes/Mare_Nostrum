348464320187c83f7b75c0b1a6061d49fd8e9546
Townes <59765081+GeneralTownes@users.noreply.github.com>
2021-12-13 22:41:19 +0000

    Dynamic printing to screen
    With aid of Sam

--- MareNostrum.py
+++ MareNostrum.py
@@ -18,13 +18,20 @@ islandSummer = pg.image.load(r'C:\\Program Files (x86)\\MareNostrum\\gfx\\town\\
 
 homeDirectory = os.getcwd()
 
+buildingSlots = 16
+
+sites = [None] * 16
 
 def Background(x,y):
     gameDisplay.blit(islandSummer, (x,y))
+    
+Locations = ((838,398), (600,394))
 
 
 class Building:
+    
     def __init__(self, buildingType, name, location, level):
+        
         self.buildingType = buildingType
         self.name = name
         self.location = location
@@ -33,11 +40,23 @@ class Building:
     def buildingGFX (self):
         buildingImage = pg.image.load( os.path.join(homeDirectory, 'gfx\\town\\buildings\\', self.name + '.png') ).convert_alpha()
         return buildingImage
+    
+# creating build list
+buildingOrder = ["townHall", "palace"]
 
-townHall = Building('standard','townHall',(838,398), 1)
+buildings = { }
+
+for i in range(len(buildingOrder)):
+    site = buildingOrder[i]
+    buildings[site] = Building('standard', site, Locations[i], 1)
+        
+    
+    
+
+print (buildings)
 
 #clickable area
-click_rect  = townHall.buildingGFX().get_rect()
+click_rect  = buildings['townHall'].buildingGFX().get_rect()
 
 crashed = False
 
@@ -48,29 +67,26 @@ while not crashed:
         if event.type == pg.QUIT:
             crashed = True
         
-        elif ( event.type == pg.MOUSEBUTTONUP ):
-           # mouse_position = pg.mouse.get_pos()             # Location of the mouse-click
+        
+        for house in buildings.values():  
+            gameDisplay.blit(house.buildingGFX(), house.location) # paint to screen
+                
+            if( click_rect.collidepoint( np.subtract(mouse_position, house.location ) ) ):
+                #gameDisplay.blit(townHall.buildingGFX(), townHall.location) # paint to screen
+                imageGlow = pg.image.load( os.path.join(homeDirectory, 'gfx\\town\\buildings\\glow', house.name + 'Glow.png') ).convert_alpha()
+                gameDisplay.blit(imageGlow, house.location) # paint to screen
+            
+        if ( event.type == pg.MOUSEBUTTONUP ):
             if ( click_rect.collidepoint( np.subtract(mouse_position, (840,400) ) ) ):   # Was that click inside our rectangle 
                 display_width = 1386
                 display_height = 924
                 gameDisplay = pg.display.set_mode((display_width,display_height))
                 islandSummer = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\island\background\Island1_Summer(1386x924).jpg')
-                
-            else:
-                pass
         
-        elif ( click_rect.collidepoint( np.subtract(mouse_position, (840,400) ) ) ):
-            gameDisplay.blit(townHall.buildingGFX(), (838,398)) # paint to screen
-            townHall_glow = pg.image.load(r'C:\Program Files (x86)\MareNostrum\gfx\town\buildings\Glow\townHallGlow.png').convert_alpha()
-            gameDisplay.blit(townHall_glow, (838,398)) # paint to screen
-        
-        else:
-            gameDisplay.blit(townHall.buildingGFX(), (838,398)) # paint to screen
-            
+        #print (event)
         
     pg.display.update()
     clock.tick(60)
-
 
 
 pg.quit()
